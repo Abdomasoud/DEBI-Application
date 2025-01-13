fba47996372a4038264a5ae4f7c84bec5399eeb5
Abdomasoud <bodymas333@gmail.com>
2025-01-13 11:12:28 +0200

    Home page & logout

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,13 +11,19 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     private static Scene scene;
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 600;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary").load(), 800, 600);
+        scene = new Scene(loadFXML("primary").load(), WIDTH, HEIGHT);
         stage.setScene(scene);
-        stage.setTitle("Fakebook Application");
+        stage.setTitle("Facebook Application");
         stage.getIcons().add(new Image(App.class.getResourceAsStream("icon.png")));
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
+        stage.setMinWidth(WIDTH);
+        stage.setMinHeight(HEIGHT);
         stage.show();
     }
 
